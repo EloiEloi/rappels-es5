de4b220e92a53f6b663dbe660d78fd7aae8503f8
Eloi Turpin <eloi.turpin@gmail.com>
2019-09-17 13:13:43 +0200

    fin tp 1

--- rappels-es5/02.tableaux.js
+++ rappels-es5/02.tableaux.js
@@ -6,4 +6,28 @@ villes.forEach(element => {
 });
 
 
-console.log(villes.every());
+var lettreADansToutesLesVilles = villes.every(
+    function (val) {
+        return val.indexOf("a") !== -1
+    });
+console.log(lettreADansToutesLesVilles);
+
+var auMoinsUneVilleAvecUnTiret = villes.some(
+    function (val) {
+        return val.indexOf("-") !== -1
+    });
+console.log(auMoinsUneVilleAvecUnTiret);
+
+var villesSansTiretSansEspace = villes.filter(
+    function (val) {
+        return val.indexOf("-") === -1 && val.indexOf(" ") === -1
+    });
+console.log(villesSansTiretSansEspace);
+
+var villesMajusculeSeTerminantParS = villes.filter(
+    function (val) {
+        return val.lastIndexOf("s") === val.length - 1
+    }).map(function (val) {
+        return val.toUpperCase()
+    });
+console.log(villesMajusculeSeTerminantParS);
